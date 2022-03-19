46b45bf4bbbb14306fbdecf9a87f6046c0d65535
Jakub Miśtal <jakub.mistal@microsoft.wsei.edu.pl>
2022-03-19 17:08:41 +0100

    copia i komentarz

---
+++ lab2/lab2/lab2/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    class Teacher
+    {
+        class Teacher
+        {
+            private string name;
+            private int age;
+
+            public Teacher(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            public bool Equals(Teacher other)
+            {
+                if (other == null) return false;
+                if (other == this) return true;
+
+                return Object.Equals(this.name, other.name) && Object.Equals(this.age, other.age);
+            }
+        }
+    }
+}
